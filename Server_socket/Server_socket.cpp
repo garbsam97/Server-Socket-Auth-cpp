f7063853eb9aa2a854389109c32733308ca75eb9
garbsam97 <samuele.garbuglia97@gmail.com>
2017-03-27 14:37:03 +0200

    wrote server socket

--- Server_socket/Server_socket.cpp
+++ Server_socket/Server_socket.cpp
@@ -2,10 +2,39 @@
 //
 
 #include "stdafx.h"
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+#pragma comment(lib, "Ws2_32.lib")
+#define DEF_PORT 48018
 
 
 int main()
 {
-    return 0;
+	SOCKET client_socket, server_socket;
+	struct sockaddr_in server, client;
+
+	//Creazione socket
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket == -1) {
+		cout << "Errore nella creazione della socket!" << endl;
+		return server_socket;
+	}
+
+	//Preparazione struttura sockaddr_in
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(DEF_PORT);
+
+	//Assegnazione indirizzo ip alla socket
+	if (bind(server_socket, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		cout << "Bind fallito!" << endl;
+		return 1;
+	}
+	return 0;
 }
 
